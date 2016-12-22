8c8475acb0229b55c6ab996a902c5457cd659f71
FabledGuitarHero <natehenderson26@gmail.com>
2016-12-22 18:58:00 -0500

    removed arbitrary code

--- TemplateMaker.php
+++ TemplateMaker.php
@@ -9,38 +9,27 @@
 		protected $template_directory = "../templates/";
 		protected $searchResults = array();
 		
-		public function __construct($template_directory = null)
-		{
-			{
-				if ($this->template_directory !== null)
+		public function __construct($template_directory = null){
+			if ($this->template_directory !== null)
 				$this->template_directory = $this->template_directory;
-			}
 		}
 		
-		public function render($template_file)
-		{
+		public function render($template_file){
 			if (file_exists($this->template_directory.$template_file))
-			{
 				include $this->template_directory.$template_file;
-			}
 			else
-			{
 				throw new Exception ("Directory Error");
-			}
 		}
 		
-		public function setResults($location, $array)
-		{
+		public function setResults($location, $array){
 			$this->searchResults[$location] = $array;
 		}
 		
-		public function fetchResults($location)
-		{
+		public function fetchResults($location){
 			return $this->searchResults[$location];
 		}
 		
-		public function fetchSpecific($location, $item)
-		{
+		public function fetchSpecific($location, $item){
 			$temp = $this->searchResults[$location];
 			return $temp[$item];
 		}
